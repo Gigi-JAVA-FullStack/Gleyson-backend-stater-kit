7328a6febab3b3d3c24a4bf780492ee000d56d9f
Gleyson Sampaio <gleyson@digytal.com.br>
2023-04-08 10:30:56 -0300

    lombok

--- spring/springboot-crud-api/src/main/java/digytal/backend/api/model/ClienteEntity.java
+++ spring/springboot-crud-api/src/main/java/digytal/backend/api/model/ClienteEntity.java
@@ -1,4 +1,33 @@
 package digytal.backend.api.model;
 
+import digytal.backend.api.enums.Sexo;
+import lombok.AccessLevel;
+import lombok.Data;
+import lombok.Setter;
+
+import javax.persistence.*;
+import java.time.LocalDate;
+
+@Data
 public class ClienteEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Setter(AccessLevel.NONE)
+    private Integer id;
+
+    @Column(length = 50, nullable = false)
+    private String nome;
+
+    @Column(length = 15, nullable = false)
+    private String cpf;
+
+    @Column(name = "dt_nascimento", nullable = false)
+    private LocalDate dataNascimento;
+
+    @Column(name = "renda_mensal", nullable = false, length = 9, precision = 2)
+    private Double rendaMensal;
+
+    @Column(length = 10, nullable = false)
+    private Sexo sexo;
+
 }
