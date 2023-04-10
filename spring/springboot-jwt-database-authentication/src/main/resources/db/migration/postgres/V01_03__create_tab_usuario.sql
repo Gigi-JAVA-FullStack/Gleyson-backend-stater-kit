1cd87a7cdb89491809130622eba0398340debb46
Gleyson Sampaio <gleyson@digytal.com.br>
2023-04-10 16:39:06 -0300

    jwt database

--- spring/springboot-jwt-database-authentication/src/main/resources/db/migration/postgres/V01_03__create_tab_usuario.sql
+++ spring/springboot-jwt-database-authentication/src/main/resources/db/migration/postgres/V01_03__create_tab_usuario.sql
@@ -5,3 +5,6 @@ create table tab_usuario
     login         varchar(20)      not null,
     senha         varchar(100)      not null,
 );
+
+insert into tab_usuario (nome, login, senha)
+values ('GLEYSON SAMPAIO','gleyson','$2a$10$YAcQ2rAN8TjoXTFCKIKww.3hCmwKh1.v1s8AJV5XrAgR.3rVvZPrK');
