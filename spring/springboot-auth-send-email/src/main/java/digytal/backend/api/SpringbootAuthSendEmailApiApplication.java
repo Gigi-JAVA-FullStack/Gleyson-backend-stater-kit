c9104f0828834a4fb1750873db4dc6f9ec3f4dc3
Gleyson Sampaio <gleyson@digytal.com.br>
2023-04-12 11:21:27 -0300

    send-email

--- spring/springboot-auth-send-email/src/main/java/digytal/backend/api/SpringbootAuthSendEmailApiApplication.java
+++ spring/springboot-auth-send-email/src/main/java/digytal/backend/api/SpringbootAuthSendEmailApiApplication.java
@@ -5,12 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class SpringbootAuthSendEmailApiApplication {
-
 	public static void main(String[] args) {
 		SpringApplication.run(SpringbootAuthSendEmailApiApplication.class, args);
-		//System.out.println(new BCryptPasswordEncoder().encode("UserP@ss"));
-		//System.out.println(new BCryptPasswordEncoder().encode("M@nager"));
 	}
-
-
 }
