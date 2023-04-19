c1374c566c79d9880eef12381ba7ac4fb3a5234e
Gleyson Sampaio <gleyson@digytal.com.br>
2023-04-19 14:43:12 -0300

    criando cadastro

--- spring/springboot-mercado-pago-integration/src/main/java/digytal/backend/api/properties/MercadoPagoCredencial.java
+++ spring/springboot-mercado-pago-integration/src/main/java/digytal/backend/api/properties/MercadoPagoCredencial.java
@@ -1,4 +1,15 @@
 package digytal.backend.api.properties;
 
+import lombok.Data;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@ConfigurationProperties(prefix = "mpc")
+@Data
 public class MercadoPagoCredencial {
+    private String accessToken;
+    private String clientId;
+    private String clientSecret;
+    private String publicKey;
 }
